7a9eb8da8e2463a5977c11d067908e8720182f27
Alex Pinedo <31834217+AlexisPinedo@users.noreply.github.com>
2019-09-28 17:43:09 -0700

    Dino Auto Run

---
+++ Dino Auto Run/Assets/PlatformMovement.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformMovement : MonoBehaviour
+{
+
+    private Rigidbody2D platformRigidbody2D;
+    
+    
+    [SerializeField]
+    private float platformSpeed = 5;
+    
+
+    private void Awake()
+    {
+        platformRigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        transform.position += Vector3.left * platformSpeed * Time.timeScale;
+        
+        //platformRigidbody2D.velocity = Vector2.left * platformSpeed;
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        PlayerMovement.playerLanded = true;
+        Debug.Log("landed");
+    }
+}
---
+++ Dino Auto Run/Assets/PlayerMovement.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Cinemachine;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private Rigidbody2D playerRigidBody;
+
+    [SerializeField] private float jumpForce;
+
+    [SerializeField] private float fallMultiplier = 2.5f;
+
+    [SerializeField] private float lowJumpMultiplier = 2f;
+    
+    [SerializeField] private float playerSpeed = 2f;
+
+    public static bool playerLanded;
+
+    private void Awake()
+    {
+        playerRigidBody = GetComponentInChildren<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        PlayerAutoRun();
+//        if (playerRigidBody.velocity.y < 0)
+//        {
+//            playerRigidBody.velocity += Vector2.up * Physics2D.gravity.y * (fallMultiplier - 1) * Time.deltaTime;
+//        }
+//        else if (playerRigidBody.velocity.y > 0 && Input.GetButton("Jump"))
+//        {
+//            playerRigidBody.velocity += Vector2.up * Physics2D.gravity.y * (lowJumpMultiplier - 1) * Time.deltaTime;
+//
+//        }
+    }
+
+    void PlayerAutoRun()
+    {
+        playerRigidBody.velocity = Vector2.right * playerSpeed;
+    }
+
+}
